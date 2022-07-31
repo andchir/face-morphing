d5bb46b0159f6581539af37fd60b0658157269a4
andrew <andchir@gmail.com>
2022-07-31 19:50:17 +0300

    save frames to directory

--- morphing/__init__.py
+++ morphing/__init__.py
@@ -1,34 +1,30 @@
-from morphing.face_landmark_detection import makeCorrespondence
-from morphing.delaunay import makeDelaunay
-from morphing.faceMorph import makeMorphs
+import os
+import sys
 import subprocess
 import argparse
 import shutil
-import os
 
-def doMorphing(thePredictor,theImage1,theImage2,theDuration,theFrameRate,theResult):
-	[size,img1,img2,list1,list2,list3]=makeCorrespondence(thePredictor,theImage1,theImage2)
-	if(size[0]==0):
-		print("Sorry, but I couldn't find a face in the image "+size[1])
-		return
-	list4=makeDelaunay(size[1],size[0],list3)
-	makeMorphs(theDuration,theFrameRate,img1,img2,list1,list2,list4,size,theResult)
+from face_landmark_detection import makeCorrespondence
+from delaunay import makeDelaunay
+from faceMorph import makeMorphs
 
-if __name__ == "__main__":
 
-	parser=argparse.ArgumentParser()
-	parser.add_argument("which", help="Face Or Body Detection?")
-	parser.add_argument("img1", help="The First Image")
-	parser.add_argument("img2", help="The Second Image")
-	parser.add_argument("dur",type=int, help="The Duration")
-	parser.add_argument("fr",type=int, help="The Frame Rate")
-	parser.add_argument("res", help="The Resultant Video")
-	args=parser.parse_args()
+def do_morphing(predictor, img1, img2, dur_frames, result_dir):
+    [size, img1, img2, list1, list2, list3] = makeCorrespondence(predictor, img1, img2)
+    if size[0] == 0:
+        print('Sorry, but I couldn\'t find a face in the image ' + size[1])
+        return
+    list4 = makeDelaunay(size[1], size[0], list3)
+    makeMorphs(dur_frames, img1, img2, list1, list2, list4, size, result_dir)
 
-	if(args.which=="face"):
-		with open(args.img1,'rb') as image1, open(args.img2,'rb') as image2:
-			doMorphing('shape_predictor_68_face_landmarks.dat',image1,image2,args.dur,args.fr,args.res)
-	elif(args.which=="body"):
-		print("Body Detection Coming Soon!")
-	else:
-		print("Please enter correct detection type.")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('img1', help='The First Image')
+    parser.add_argument('img2', help='The Second Image')
+    parser.add_argument('dur_frames', type=int, help='The Duration in frames')
+    parser.add_argument('res', help='The Resultant Video')
+    args = parser.parse_args()
+
+    with open(args.img1, 'rb') as image1, open(args.img2, 'rb') as image2:
+        do_morphing('shape_predictor_68_face_landmarks.dat', image1, image2, args.dur_frames, args.res)
--- morphing/faceMorph.py
+++ morphing/faceMorph.py
@@ -6,42 +6,39 @@ import math
 from subprocess import Popen, PIPE
 from PIL import Image
 
+
 # Apply affine transform calculated using srcTri and dstTri to src and
 # output an image of size.
-def applyAffineTransform(src, srcTri, dstTri, size) :
-    
+def applyAffineTransform(src, srcTri, dstTri, size):
     # Given a pair of triangles, find the affine transform.
-    warpMat = cv2.getAffineTransform( np.float32(srcTri), np.float32(dstTri) )
-    
+    warpMat = cv2.getAffineTransform(np.float32(srcTri), np.float32(dstTri))
+
     # Apply the Affine Transform just found to the src image
-    dst = cv2.warpAffine( src, warpMat, (size[0], size[1]), None, flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT_101 )
+    dst = cv2.warpAffine(src, warpMat, (size[0], size[1]), None, flags=cv2.INTER_LINEAR,
+                         borderMode=cv2.BORDER_REFLECT_101)
 
     return dst
 
 
 # Warps and alpha blends triangular regions from img1 and img2 to img
-def morphTriangle(img1, img2, img, t1, t2, t, alpha) :
-
+def morphTriangle(img1, img2, img, t1, t2, t, alpha):
     # Find bounding rectangle for each triangle
     r1 = cv2.boundingRect(np.float32([t1]))
     r2 = cv2.boundingRect(np.float32([t2]))
     r = cv2.boundingRect(np.float32([t]))
-
 
     # Offset points by left top corner of the respective rectangles
     t1Rect = []
     t2Rect = []
     tRect = []
 
-
     for i in range(0, 3):
-        tRect.append(((t[i][0] - r[0]),(t[i][1] - r[1])))
-        t1Rect.append(((t1[i][0] - r1[0]),(t1[i][1] - r1[1])))
-        t2Rect.append(((t2[i][0] - r2[0]),(t2[i][1] - r2[1])))
-
+        tRect.append(((t[i][0] - r[0]), (t[i][1] - r[1])))
+        t1Rect.append(((t1[i][0] - r1[0]), (t1[i][1] - r1[1])))
+        t2Rect.append(((t2[i][0] - r2[0]), (t2[i][1] - r2[1])))
 
     # Get mask by filling triangle
-    mask = np.zeros((r[3], r[2], 3), dtype = np.float32)
+    mask = np.zeros((r[3], r[2], 3), dtype=np.float32)
     cv2.fillConvexPoly(mask, np.int32(tRect), (1.0, 1.0, 1.0), 16, 0);
 
     # Apply warpImage to small rectangular patches
@@ -56,16 +53,16 @@ def morphTriangle(img1, img2, img, t1, t2, t, alpha) :
     imgRect = (1.0 - alpha) * warpImage1 + alpha * warpImage2
 
     # Copy triangular region of the rectangular patch to the output image
-    img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] = img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] * ( 1 - mask ) + imgRect * mask
+    img[r[1]:r[1] + r[3], r[0]:r[0] + r[2]] = img[r[1]:r[1] + r[3], r[0]:r[0] + r[2]] * (1 - mask) + imgRect * mask
 
 
-def makeMorphs(theDuration,theFrameRate,theImage1,theImage2,theList1,theList2,theList4,size,theResult):
+def makeMorphs(totalImages, theImage1, theImage2, theList1, theList2, theList4, size, theResultDir):
 
-    totalImages=int(theDuration*theFrameRate)
+    if not os.path.isdir(theResultDir):
+        os.mkdir(theResultDir)
 
-    p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-r', str(theFrameRate),'-s',str(size[1])+'x'+str(size[0]), '-i', '-', '-c:v', 'libx264', '-crf', '25','-vf','scale=trunc(iw/2)*2:trunc(ih/2)*2','-pix_fmt','yuv420p', theResult], stdin=PIPE)
-    for j in range(0,totalImages):
-        
+    for j in range(0, totalImages):
+
         # Read images
         img1 = theImage1
         img2 = theImage2
@@ -77,36 +74,35 @@ def makeMorphs(theDuration,theFrameRate,theImage1,theImage2,theList1,theList2,th
         # Read array of corresponding points
         points1 = theList1
         points2 = theList2
-        points = [];
-        alpha=j/(totalImages-1)
+        points = []
+        alpha = j / (totalImages - 1)
 
         # Compute weighted average point coordinates
         for i in range(0, len(points1)):
-            x = ( 1 - alpha ) * points1[i][0] + alpha * points2[i][0]
-            y = ( 1 - alpha ) * points1[i][1] + alpha * points2[i][1]
-            points.append((x,y))
-
+            x = (1 - alpha) * points1[i][0] + alpha * points2[i][0]
+            y = (1 - alpha) * points1[i][1] + alpha * points2[i][1]
+            points.append((x, y))
 
         # Allocate space for final output
-        imgMorph = np.zeros(img1.shape, dtype = img1.dtype)
+        imgMorph = np.zeros(img1.shape, dtype=img1.dtype)
 
         # Read triangles from delaunay_output.txt
-        for i in range(len(theList4)):    
+        for i in range(len(theList4)):
             x = int(theList4[i][0])
             y = int(theList4[i][1])
             z = int(theList4[i][2])
-            
+
             t1 = [points1[x], points1[y], points1[z]]
             t2 = [points2[x], points2[y], points2[z]]
-            t = [ points[x], points[y], points[z] ]
+            t = [points[x], points[y], points[z]]
 
             # Morph one triangle at a time.
             morphTriangle(img1, img2, imgMorph, t1, t2, t, alpha)
 
-        temp_res=cv2.cvtColor(np.uint8(imgMorph),cv2.COLOR_BGR2RGB)
-        res=Image.fromarray(temp_res)
-        res.save(p.stdin,'JPEG')
+        temp_res = cv2.cvtColor(np.uint8(imgMorph), cv2.COLOR_BGR2RGB)
+        res = Image.fromarray(temp_res)
 
-    p.stdin.close()
-    p.wait()
+        frame_number = j + 1
+        res.save(os.path.join(theResultDir, f'{frame_number:06}' + '.jpg'), 'JPEG')
+
 #  makeMorphs(0.5,60,'2.jpg','3.jpg')
